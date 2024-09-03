9fc199bec5181d914e9ccc54f89cf87dc67279ad
Rohan Agarwal <21052610@kiit.ac.in>
2024-09-04 02:31:44 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,51 +1,94 @@
 // @flow strict
+import { personalData } from '@/utils/data/personal-data';
 import Link from 'next/link';
-import { FaArrowRight } from 'react-icons/fa';
-import BlogCard from './blog-card';
+import { BiLogoLinkedin } from "react-icons/bi";
+import { CiLocationOn } from "react-icons/ci";
+import { FaFacebook, FaStackOverflow } from 'react-icons/fa';
+import { FaXTwitter } from "react-icons/fa6";
+import { IoLogoGithub, IoMdCall } from "react-icons/io";
+import { MdAlternateEmail } from "react-icons/md";
+import ContactWithCaptcha from './contact-with-captcha';
+import ContactWithoutCaptcha from './contact-without-captcha';
 
-function Blog({ blogs }) {
-
+function ContactSection() {
   return (
-    <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
-      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
-
-      <div className="flex justify-center -translate-y-[1px]">
-        <div className="w-3/4">
-          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
-        </div>
+    <div id="contact" className="my-12 lg:my-16 relative mt-24 text-white">
+      <div className="hidden lg:flex flex-col items-center absolute top-24 -right-8">
+        <span className="bg-[#1a1443] w-fit text-white rotate-90 p-2 px-5 text-xl rounded-md">
+          CONTACT
+        </span>
+        <span className="h-36 w-[2px] bg-[#1a1443]"></span>
       </div>
-
-      <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
-            Blogs
-          </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-        </div>
-      </div>
-
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16 items-center">
         {
-          blogs.slice(0, 6).map((blog, i) => (
-            blog?.cover_image &&
-            <BlogCard blog={blog} key={i} />
-          ))
+          (process.env.NEXT_PUBLIC_RECAPTCHA_SITE_KEY && process.env.NEXT_PUBLIC_RECAPTCHA_SECRET_KEY) ? <ContactWithCaptcha />
+            : <ContactWithoutCaptcha />
         }
-      </div>
 
-      <div className="flex justify-center  mt-5 lg:mt-12">
-        <Link
-          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
-          role="button"
-          href="/blog"
-        >
-          <span>View More</span>
-          <FaArrowRight size={16} />
-        </Link>
+        <div className="lg:w-3/4 ">
+          <div className="flex flex-col gap-5 lg:gap-9">
+            <p className="text-sm md:text-xl flex items-center gap-3">
+              <MdAlternateEmail
+                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={36}
+              />
+              <span>{personalData.email}</span>
+            </p>
+            <p className="text-sm md:text-xl flex items-center gap-3">
+              <IoMdCall
+                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={36}
+              />
+              <span>
+                {personalData.phone}
+              </span>
+            </p>
+            <p className="text-sm md:text-xl flex items-center gap-3">
+              <CiLocationOn
+                className="bg-[#8b98a5] p-2 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={36}
+              />
+              <span>
+                {personalData.address}
+              </span>
+            </p>
+          </div>
+          <div className="mt-8 lg:mt-16 flex items-center gap-5 lg:gap-10">
+            <Link target="_blank" href={personalData.github}>
+              <IoLogoGithub
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+            <Link target="_blank" href={personalData.linkedIn}>
+              <BiLogoLinkedin
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+            <Link target="_blank" href={personalData.twitter}>
+              <FaXTwitter
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+            <Link target="_blank" href={personalData.stackOverflow}>
+              <FaStackOverflow
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+            <Link target="_blank" href={personalData.facebook}>
+              <FaFacebook
+                className="bg-[#8b98a5] p-3 rounded-full hover:bg-[#16f2b3] hover:scale-110 transition-all duration-300 text-gray-800 cursor-pointer"
+                size={48}
+              />
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
 };
 
-export default Blog;
+export default ContactSection;
