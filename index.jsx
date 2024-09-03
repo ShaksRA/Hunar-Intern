6ee95d3025fdd9b09f6545ecfe7bd56401efbe15
Rohan Agarwal <21052610@kiit.ac.in>
2024-09-04 02:31:21 +0530

    Add files via upload

--- index.jsx
+++ index.jsx
@@ -1,39 +1,51 @@
 // @flow strict
+import Link from 'next/link';
+import { FaArrowRight } from 'react-icons/fa';
+import BlogCard from './blog-card';
 
-import { personalData } from "@/utils/data/personal-data";
-import Image from "next/image";
+function Blog({ blogs }) {
 
-
-function AboutSection() {
   return (
-    <div id="about" className="my-12 lg:my-16 relative">
-      <div className="hidden lg:flex flex-col items-center absolute top-16 -right-8">
-        <span className="bg-[#1a1443] w-fit text-white rotate-90 p-2 px-5 text-xl rounded-md">
-          ABOUT ME
-        </span>
-        <span className="h-36 w-[2px] bg-[#1a1443]"></span>
+    <div id='blogs' className="relative z-50 border-t my-12 lg:my-24 border-[#25213b]">
+      <div className="w-[100px] h-[100px] bg-violet-100 rounded-full absolute top-6 left-[42%] translate-x-1/2 filter blur-3xl  opacity-20"></div>
+
+      <div className="flex justify-center -translate-y-[1px]">
+        <div className="w-3/4">
+          <div className="h-[1px] bg-gradient-to-r from-transparent via-violet-500 to-transparent  w-full" />
+        </div>
       </div>
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-8 lg:gap-16">
-        <div className="order-2 lg:order-1">
-          <p className="font-medium mb-5 text-[#16f2b3] text-xl uppercase">
-            Who I am?
-          </p>
-          <p className="text-gray-200 text-sm lg:text-lg">
-            {personalData.description}
-          </p>
+
+      <div className="flex justify-center my-5 lg:py-8">
+        <div className="flex  items-center">
+          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-xl rounded-md">
+            Blogs
+          </span>
+          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
         </div>
-        <div className="flex justify-center order-1 lg:order-2">
-          <Image
-            src={personalData.profile}
-            width={280}
-            height={280}
-            alt="Abu Said"
-            className="rounded-lg transition-all duration-1000 grayscale hover:grayscale-0 hover:scale-110 cursor-pointer"
-          />
-        </div>
+      </div>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 md:gap-5 lg:gap-8 xl:gap-10">
+        {
+          blogs.slice(0, 6).map((blog, i) => (
+            blog?.cover_image &&
+            <BlogCard blog={blog} key={i} />
+          ))
+        }
+      </div>
+
+      <div className="flex justify-center  mt-5 lg:mt-12">
+        <Link
+          className="flex items-center gap-1 hover:gap-3 rounded-full bg-gradient-to-r from-pink-500 to-violet-600 px-3 md:px-8 py-3 md:py-4 text-center text-xs md:text-sm font-medium uppercase tracking-wider text-white no-underline transition-all duration-200 ease-out hover:text-white hover:no-underline md:font-semibold"
+          role="button"
+          href="/blog"
+        >
+          <span>View More</span>
+          <FaArrowRight size={16} />
+        </Link>
       </div>
     </div>
   );
 };
 
-export default AboutSection;
+export default Blog;
